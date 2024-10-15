ba4ead5a0f59ab08538fc01c8b41f529e66efefc
Chaits-ctrl <chaitrali.ghule22@vit.edu>
2024-10-15 17:49:36 +0530

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,16 +1,18 @@
-"""
-Service Package
-"""
 from flask import Flask
 
 app = Flask(__name__)
 
-# This must be imported after the Flask app is created
+# Importing after creating the Flask app
 from service import routes              
 from service.common import log_handlers 
 
-log_handlers.init_logging(app, "gunicorn.error")
+# Initialize logging with gunicorn error log
+log_handlers.init_logging(app, log_name="gunicorn.error")
 
-app.logger.info(70 * "*")
-app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
-app.logger.info(70 * "*")
+# Logging service start message
+app.logger.info("*" * 70)
+app.logger.info(" S E R V I C E   R U N N I N G ".center(70, "*"))
+app.logger.info("*" * 70)
+
+if __name__ == "__main__":
+    app.run()
