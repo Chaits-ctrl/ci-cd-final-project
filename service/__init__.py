a53342ea9888f9daa0a55116d2ddb6757df86bc3
Chaits-ctrl <chaitrali.ghule22@vit.edu>
2024-10-15 17:55:39 +0530

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -1,18 +1,16 @@
+"""
+Service Package
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
-# Importing after creating the Flask app
-from service import routes              
-from service.common import log_handlers 
+# This must be imported after the Flask app is created
+from service import routes               # pylint: disable=wrong-import-position,cyclic-import
+from service.common import log_handlers  # pylint: disable=wrong-import-position
 
-# Initialize logging with gunicorn error log
-log_handlers.init_logging(app, log_name="gunicorn.error")
+log_handlers.init_logging(app, "gunicorn.error")
 
-# Logging service start message
-app.logger.info("*" * 70)
-app.logger.info(" S E R V I C E   R U N N I N G ".center(70, "*"))
-app.logger.info("*" * 70)
-
-if __name__ == "__main__":
-    app.run()
+app.logger.info(70 * "*")
+app.logger.info("  S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(70 * "*")
