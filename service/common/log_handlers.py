6063fdb23671beb6a78aa9bd349aaa2adae8341b
Chaits-ctrl <chaitrali.ghule22@vit.edu>
2024-10-15 18:19:34 +0530

    Update log_handlers.py

--- service/common/log_handlers.py
+++ service/common/log_handlers.py
@@ -1,38 +1,33 @@
-######################################################################
-# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-######################################################################
-
 """
 Log Handlers
 
 This module contains utility functions to set up logging
-consistently
+consistently.
 """
 import logging
 
 
 def init_logging(app, logger_name: str):
     """Set up logging for production"""
+
+    # Ensure logs from app.logger don't propagate to the root logger
     app.logger.propagate = False
+
+    # Get the gunicorn logger
     gunicorn_logger = logging.getLogger(logger_name)
+
+    # Set app logger's handlers and level to match gunicorn's logger
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
-    # Make all log formats consistent
+
+    # Ensure log formats are consistent across all handlers
     formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", "%Y-%m-%d %H:%M:%S %z"
+        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s",
+        "%Y-%m-%d %H:%M:%S %z"
     )
+
     for handler in app.logger.handlers:
         handler.setFormatter(formatter)
+
+    # Log that the logging handler has been successfully established
     app.logger.info("Logging handler established")
